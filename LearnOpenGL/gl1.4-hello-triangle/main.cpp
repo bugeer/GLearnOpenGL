1f61fd79abb48c9a12c2c310d6da5445f8544c8a
zhangjing5 <zhangjing5@sensetime.com>
2024-04-22 12:20:24 +0800

    ebo & key event.

--- LearnOpenGL/gl1.4-hello-triangle/main.cpp
+++ LearnOpenGL/gl1.4-hello-triangle/main.cpp
@@ -2,14 +2,31 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+enum RenderMode {
+	POLYGON  = 0,
+	POLYLINE = 1,
+};
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	static RenderMode rm = RenderMode::POLYGON;
+	if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+		rm = rm == RenderMode::POLYGON ? RenderMode::POLYLINE : RenderMode::POLYGON;
+		if (rm == RenderMode::POLYGON) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			std::cout << "PLOYLGON" << std::endl;
+		}
+		else if (rm == RenderMode::POLYLINE) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			std::cout << "PLOYLLINE" << std::endl;
+		}
+	}
+	else if (key == GLFW_KEY_ESCAPE) {
         glfwSetWindowShouldClose(window, true);
+	}
 }
 
 const char* vertexShaderSoruce = "#version 330 core\n"
@@ -87,37 +104,46 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+
 	float vertices[] = {
 		-0.5f, -0.5f, 0.0f,
 		+0.5f, -0.5f, 0.0f,
 		+0.0f, +0.5f, 0.0f,
+		+1.0f, +0.5f, 0.0f,
+	};
+	unsigned int indices[] = {
+		0, 1, 2,
+		1, 2, 3
 	};
 	unsigned int VBO;
 	unsigned int VAO;
-	glGenBuffers(1, &VBO);
+	unsigned int EBO;
 	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
+	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	glUseProgram(shaderProgram);
-
+	glfwSetKeyCallback(window, key_callback);
 
 	while (!glfwWindowShouldClose(window)) {
-		processInput(window);
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBindVertexArray(VAO);
 		glUseProgram(shaderProgram);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAO);
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -125,6 +151,7 @@ int main() {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
